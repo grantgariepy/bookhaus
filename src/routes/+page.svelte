9b35378d4b3bafe31069406b4e067d824a7170bb
grantgariepy <grantgariepy@protonmail.com>
2022-12-26 16:56:46 -0700

    nav and footer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 </script>
 
-<div class="bg-gray-800 text-white">
+<div class="bg-zinc-800 text-white">
 	<h1>Welcome to SvelteKit</h1>
 	<p>
 		Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a
