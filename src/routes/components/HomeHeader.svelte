a0953aa5f21eda4b73b30ea3619186d71a12d4d1
grantgariepy <grantgariepy@protonmail.com>
2023-01-09 20:23:52 -0700

    search bar fix

--- src/routes/components/HomeHeader.svelte
+++ src/routes/components/HomeHeader.svelte
@@ -53,10 +53,30 @@
 						class="pt-2 text-gray-300 hover:text-white text-md font-semibold transition duration-100"
 						>JOURNALS</a
 					>
-					<input
-						type="text"
-						class="form-control opacity-50 bg-zinc-300  text-zinc-400 focus:text-zinc-900 rounded-3xl border-b border-zinc-500 focus:bg-white  focus:outline-none px-2"
-					/>
+					<div class="flex group">
+						<input
+							type="text"
+							class="form-control opacity-50 bg-zinc-300  text-zinc-400 focus:text-zinc-900 rounded-3xl border-b border-zinc-500  active:bg-white hover:bg-white focus:opacity-100 focus:outline-none my-1 pl-2"
+						/>
+						<div
+							class="absolute right-2 pt-3  cursor-pointer"
+						>
+							<svg
+								aria-hidden="true"
+								focusable="false"
+								data-prefix="fas"
+								data-icon="search"
+								class="w-3 fill-white group-active:fill-black group-focus-within:fill-zinc-700"
+								role="img"
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 512 512"
+							>
+								<path
+									d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"
+								/>
+							</svg>
+						</div>
+					</div>
 				</nav>
 				<!-- nav - end -->
 			</header>
