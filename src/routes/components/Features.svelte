90eb832cbd00fb7d83d738a01d0f112801cc5b82
grantgariepy <grantgariepy@protonmail.com>
2023-01-14 13:44:00 -0700

    features icons

--- src/routes/components/Features.svelte
+++ src/routes/components/Features.svelte
@@ -15,49 +15,143 @@
 			class="grid md:grid-cols-3 rounded-sm gap-3 lg:gap-2 "
 		>
 			<div
-				class="transition duration-150 h-28 md:h-42 flex justify-center items-center bg-zinc-500 cursor-pointer hover:bg-green-500 text-zinc-200 rounded-md p-4"
+				class="group transition duration-150 h-28 md:h-42 flex justify-center items-center bg-zinc-500 cursor-pointer hover:bg-green-500  rounded-sm p-2"
 			>
-				<p>
+				<div class="pl-2 pr-4 pb-6 ">
+					<svg
+						fill="currentColor"
+						class="w-12 h-12 text-zinc-300 self-center group-hover:text-white transition duration-150"
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 512.001 512.001"
+					>
+						<path
+							d="M256,192.012c-35.313,0-64.002,28.688-64.002,64s28.689,63.969,64.002,63.969s64-28.656,64-63.969 S291.313,192.012,256,192.012z"
+						/>
+						<path
+							d="M505.381,236.512c-56.082-72.938-125.065-140.438-249.225-140.5h-0.016h-0.016c0-0.031-0.094-0.031-0.125,0 c0,0-0.078,0-0.109,0c-0.016,0-0.016,0-0.016,0h-0.016c-0.016,0-0.016,0-0.016,0C131.684,96.075,62.7,163.575,6.62,236.512 c-8.826,11.5-8.826,27.5,0,39c56.08,72.938,125.065,140.438,249.225,140.5c0,0,0,0,0.016,0h0.016c0,0,0,0,0.016,0h0.016 c0.016,0,0.094,0,0.094,0c0.016,0,0.031,0,0.064,0h0.029h0.016h0.016h0.016h0.016c0.109,0,0.219,0,0.344,0 c0.141-0.031,0.281-0.031,0.422,0c123.66-0.336,192.486-67.719,248.459-140.5C514.207,264.012,514.207,248.012,505.381,236.512z M256.094,352.012h-0.018h-0.012h-0.018h-0.031H256c-0.031,0-0.065,0-0.065,0h-0.014c0,0,0,0-0.016,0 c-52.879-0.063-95.908-43.125-95.908-96s43.029-95.938,95.908-96c0.029,0,0.078,0,0.141,0h0.047 c52.877,0.063,95.906,43.125,95.906,96S308.971,351.95,256.094,352.012z"
+						/>
+					</svg>
+				</div>
+				<p
+					class="text-zinc-200 group-hover:text-white transition duration-150"
+				>
 					Keep track of every book you've ever watched (or
 					just start from the day you join)
 				</p>
 			</div>
 			<div
-				class="transition duration-150 h-28 md:h-42 flex justify-center items-center bg-zinc-500 cursor-pointer hover:bg-orange-500 text-zinc-200 rounded-md p-4"
+				class="group transition duration-150 h-28 md:h-42 flex justify-center items-center bg-zinc-500 cursor-pointer hover:bg-green-500  rounded-sm p-2"
 			>
-				<p>
+				<div class="pl-2 pr-4 pb-6 ">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="w-10 h-10 text-zinc-300 self-center group-hover:text-white transition duration-150"
+						viewBox="0 0 512 512"
+						fill="currentColor"
+					>
+						<path
+							class="st0"
+							d="M473.984,74.248c-50.688-50.703-132.875-50.703-183.563,0c-17.563,17.547-29.031,38.891-34.438,61.391 c-5.375-22.5-16.844-43.844-34.406-61.391c-50.688-50.703-132.875-50.703-183.563,0c-50.688,50.688-50.688,132.875,0,183.547 l217.969,217.984l218-217.984C524.672,207.123,524.672,124.936,473.984,74.248z"
+						/>
+					</svg>
+				</div>
+				<p
+					class="text-zinc-200 group-hover:text-white transition duration-150"
+				>
 					Show some love for your favorite books, lists and
 					reviews with a “like”
 				</p>
 			</div>
 			<div
-				class="transition duration-150 h-28 md:h-42 flex justify-center items-center bg-zinc-500 cursor-pointer hover:bg-blue-500 text-zinc-200 rounded-md p-4"
+				class="group transition duration-150 h-28 md:h-42 flex justify-center items-center bg-zinc-500 cursor-pointer hover:bg-green-500  rounded-sm p-2"
 			>
-				<p>
+				<div class="pl-2 pr-4 pb-6 ">
+					<svg
+						fill="currentColor"
+						class="w-10 h-10 text-zinc-300 self-center group-hover:text-white transition duration-150"
+						viewBox="0 0 8 8"
+						xmlns="http://www.w3.org/2000/svg"
+					>
+						<path
+							d="M0 0v1h8v-1h-8zm0 2v1h6v-1h-6zm0 2v1h8v-1h-8zm0 2v1h6v-1h-6z"
+						/>
+					</svg>
+				</div>
+				<p
+					class="text-zinc-200 group-hover:text-white transition duration-150"
+				>
 					Write and share reviews, and follow friends and
 					other members to read theirs
 				</p>
 			</div>
 			<div
-				class="transition duration-150 h-28 md:h-42 flex justify-center items-center bg-zinc-500 cursor-pointer hover:bg-green-500 text-zinc-200 rounded-md p-4"
+				class="group transition duration-150 h-28 md:h-42 flex justify-center items-center bg-zinc-500 cursor-pointer hover:bg-green-500  rounded-sm p-2"
 			>
-				<p>
+				<div class="pl-2 pr-4 pb-6 ">
+					<svg
+						fill="currentColor"
+						class="w-12 h-12 text-zinc-300 self-center group-hover:text-white transition duration-150"
+						viewBox="0 0 32 32"
+						xmlns="http://www.w3.org/2000/svg"
+					>
+						<path
+							d="M30.383 12.699c-0.1-0.303-0.381-0.519-0.713-0.519-0 0-0 0-0 0h-9.898l-3.059-9.412c-0.124-0.276-0.396-0.464-0.713-0.464s-0.589 0.189-0.711 0.459l-0.002 0.005-3.059 9.412h-9.897c-0.414 0-0.749 0.336-0.749 0.75 0 0.248 0.121 0.469 0.307 0.605l0.002 0.001 8.007 5.818-3.059 9.412c-0.023 0.070-0.037 0.15-0.037 0.233 0 0.414 0.336 0.75 0.75 0.75 0.165 0 0.318-0.054 0.442-0.144l-0.002 0.001 8.008-5.818 8.006 5.818c0.122 0.090 0.275 0.144 0.441 0.144 0.414 0 0.75-0.336 0.75-0.75 0-0.083-0.014-0.164-0.039-0.239l0.002 0.005-3.059-9.412 8.010-5.818c0.188-0.138 0.308-0.357 0.308-0.605 0-0.083-0.014-0.163-0.038-0.238l0.002 0.005z"
+						/>
+					</svg>
+				</div>
+				<p
+					class="text-zinc-200 group-hover:text-white transition duration-150"
+				>
 					Rate each book on a five-star scale (with halves)
 					to record and share your reaction
 				</p>
 			</div>
 			<div
-				class="transition duration-150 h-28 md:h-42 flex justify-center items-center bg-zinc-500 cursor-pointer hover:bg-orange-500 text-zinc-200 rounded-md p-4"
+				class="group transition duration-150 h-28 md:h-42 flex justify-center items-center bg-zinc-500 cursor-pointer hover:bg-green-500  rounded-sm p-2"
 			>
-				<p>
+				<div class="pl-2 pr-4 pb-6 ">
+					<svg
+						class="w-10 h-10 text-zinc-300 self-center group-hover:text-white transition duration-150"
+						viewBox="0 0 48 48"
+						xmlns="http://www.w3.org/2000/svg"
+						fill="currentColor"
+					>
+						<rect width="48" height="48" fill="none" />
+
+						<path
+							d="M2,42a2,2,0,0,0,2,2H44a2,2,0,0,0,2-2V20H2ZM44,8H35V4.1A2.1,2.1,0,0,0,33.3,2,2,2,0,0,0,31,4V8H17V4.1A2.1,2.1,0,0,0,15.3,2,2,2,0,0,0,13,4V8H4a2,2,0,0,0-2,2v6H46V10A2,2,0,0,0,44,8Z"
+						/>
+					</svg>
+				</div>
+				<p
+					class="text-zinc-200 group-hover:text-white transition duration-150"
+				>
 					Keep a diary of your book reading (and upgrade to
 					Pro for comprehensive stats)
 				</p>
 			</div>
 			<div
-				class="transition duration-150 h-28 md:h-42 flex justify-center items-center bg-zinc-500 cursor-pointer hover:bg-blue-500 text-zinc-200 rounded-md p-4"
+				class="group transition duration-150 h-28 md:h-42 flex justify-center items-center bg-zinc-500 cursor-pointer hover:bg-green-500  rounded-sm p-2"
 			>
-				<p>
+				<div class="pl-2 pr-4 pb-6 ">
+					<svg
+						class="w-10 h-10 text-zinc-300 self-center group-hover:text-white transition duration-150"
+						viewBox="0 0 48 48"
+						xmlns="http://www.w3.org/2000/svg"
+						fill="currentColor"
+					>
+						<path d="M0 0h48v48H0z" fill="none" />
+						<g id="Shopicon">
+							<rect x="4" y="4" width="18" height="18" />
+							<rect x="26" y="4" width="18" height="18" />
+							<rect x="4" y="26" width="18" height="18" />
+							<rect x="26" y="26" width="18" height="18" />
+						</g>
+					</svg>
+				</div>
+				<p
+					class="text-zinc-200 group-hover:text-white transition duration-150"
+				>
 					Compile and share lists of books on any topic and
 					keep a watchlist of books to see
 				</p>
