f7b41169edd68f9608826dd7a1a950dccf5fb832
grantgariepy <grantgariepy@protonmail.com>
2023-01-15 21:00:52 -0700

    side module started

--- src/routes/book/BookSlug/components/Tabs.svelte
+++ src/routes/book/BookSlug/components/Tabs.svelte
@@ -1,143 +1,35 @@
-<ul
-	class="nav nav-tabs flex flex-col md:flex-row flex-wrap list-none border-b-0 pl-0 mb-4"
-	id="tabs-tab"
-	role="tablist"
+<div
+	class="flex flex-row items-center mb-2 border-b border-zinc-400"
 >
-	<li class="nav-item" role="presentation">
-		<a
-			href="#tabs-home"
-			class="
-      nav-link
-      block
-      font-medium
-      text-xs
-      leading-tight
-      uppercase
-      border-x-0 border-t-0 border-b-2 border-transparent
-      px-6
-      py-3
-      my-2
-      hover:border-transparent hover:bg-gray-100
-      focus:border-transparent
-      active
-    "
-			id="tabs-home-tab"
-			data-bs-toggle="pill"
-			data-bs-target="#tabs-home"
-			role="tab"
-			aria-controls="tabs-home"
-			aria-selected="true">Home</a
-		>
-	</li>
-	<li class="nav-item" role="presentation">
-		<a
-			href="#tabs-profile"
-			class="
-      nav-link
-      block
-      font-medium
-      text-xs
-      leading-tight
-      uppercase
-      border-x-0 border-t-0 border-b-2 border-transparent
-      px-6
-      py-3
-      my-2
-      hover:border-transparent hover:bg-gray-100
-      focus:border-transparent
-    "
-			id="tabs-profile-tab"
-			data-bs-toggle="pill"
-			data-bs-target="#tabs-profile"
-			role="tab"
-			aria-controls="tabs-profile"
-			aria-selected="false">Profile</a
-		>
-	</li>
-	<li class="nav-item" role="presentation">
-		<a
-			href="#tabs-messages"
-			class="
-      nav-link
-      block
-      font-medium
-      text-xs
-      leading-tight
-      uppercase
-      border-x-0 border-t-0 border-b-2 border-transparent
-      px-6
-      py-3
-      my-2
-      hover:border-transparent hover:bg-gray-100
-      focus:border-transparent
-    "
-			id="tabs-messages-tab"
-			data-bs-toggle="pill"
-			data-bs-target="#tabs-messages"
-			role="tab"
-			aria-controls="tabs-messages"
-			aria-selected="false">Messages</a
-		>
-	</li>
-	<li class="nav-item" role="presentation">
-		<a
-			href="#tabs-contact"
-			class="
-      nav-link
-      disabled
-      pointer-events-none
-      block
-      font-medium
-      text-xs
-      leading-tight
-      uppercase
-      border-x-0 border-t-0 border-b-2 border-transparent
-      px-6
-      py-3
-      my-2
-      hover:border-transparent hover:bg-gray-100
-      focus:border-transparent
-    "
-			id="tabs-contact-tab"
-			data-bs-toggle="pill"
-			data-bs-target="#tabs-contact"
-			role="tab"
-			aria-controls="tabs-contact"
-			aria-selected="false">Contact</a
-		>
-	</li>
-</ul>
-<div class="tab-content" id="tabs-tabContent">
-	<div
-		class="tab-pane fade show active"
-		id="tabs-home"
-		role="tabpanel"
-		aria-labelledby="tabs-home-tab"
+	<p
+		class="z-10 text-zinc-200 underline-offset-4 underline decoration-3 text-sm  text-center mb-0 uppercase tracking-widest"
 	>
-		Tab 1 content
-	</div>
-	<div
-		class="tab-pane fade"
-		id="tabs-profile"
-		role="tabpanel"
-		aria-labelledby="tabs-profile-tab"
+		Genres
+	</p>
+</div>
+<div class="flex flex-row px-5 justify-self-start">
+	<a
+		href="/"
+		class="min-w-max bg-zinc-600 mx-auto px-3 rounded hover:text-zinc-100"
 	>
-		Tab 2 content
-	</div>
-	<div
-		class="tab-pane fade"
-		id="tabs-messages"
-		role="tabpanel"
-		aria-labelledby="tabs-profile-tab"
+		Fiction
+	</a>
+	<a
+		href="/"
+		class="min-w-max bg-zinc-600 mx-auto px-3 rounded hover:text-zinc-100"
 	>
-		Tab 3 content
-	</div>
-	<div
-		class="tab-pane fade"
-		id="tabs-contact"
-		role="tabpanel"
-		aria-labelledby="tabs-contact-tab"
+		Fiction
+	</a>
+	<a
+		href="/"
+		class="min-w-max bg-zinc-600 mx-auto px-3 rounded hover:text-zinc-100"
 	>
-		Tab 4 content
-	</div>
+		Fiction
+	</a>
+	<a
+		href="/"
+		class="min-w-max bg-zinc-600 mx-auto px-3 rounded hover:text-zinc-100"
+	>
+		Fiction
+	</a>
 </div>
