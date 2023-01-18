c377140bea723c8b773c1e815c8df35b4bd1541d
grantgariepy <grantgariepy@protonmail.com>
2023-01-17 20:38:03 -0700

    raings box

--- src/routes/book/BookSlug/components/Ratings.svelte
+++ src/routes/book/BookSlug/components/Ratings.svelte
@@ -1,4 +1,4 @@
-<div class="flex flex-col bg-black ">
+<div class="flex flex-col">
 	<div
 		class="flex flex-row justify-between items-center mb-2 border-b border-zinc-400"
 	>
@@ -13,7 +13,107 @@
 			1.2K Fans
 		</p>
 	</div>
-	<p class="text-white">hello</p>
-	<p class="text-white">hello</p>
-	<p class="text-white">hello</p>
+	<div class="flex flex-row gap-1 mx-auto">
+		<div class="flex self-end">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				class="w-3 h-3 text-green-400"
+				viewBox="0 0 20 20"
+				fill="currentColor"
+			>
+				<path
+					d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+				/>
+			</svg>
+		</div>
+		<div class="flex flex-row gap-0.5">
+			<div
+				class="flex self-end bg-zinc-500 hover:bg-zinc-300 w-3 h-0.5 rounded-t-sm "
+			/>
+			<div
+				class="flex self-end bg-zinc-500 hover:bg-zinc-300 w-3 h-1 rounded-t-sm"
+			/>
+			<div
+				class="flex self-end bg-zinc-500 hover:bg-zinc-300 w-3 h-2 rounded-t-sm"
+			/>
+			<div
+				class="flex self-end bg-zinc-500 hover:bg-zinc-300 w-3 h-6 rounded-t-sm"
+			/>
+			<div
+				class="flex self-end bg-zinc-500 hover:bg-zinc-300 w-3 h-7 rounded-t-sm"
+			/>
+			<div
+				class="flex self-end bg-zinc-500 hover:bg-zinc-300 w-3 h-8 rounded-t-sm"
+			/>
+			<div
+				class="flex self-end bg-zinc-500 hover:bg-zinc-300 w-3 h-6 rounded-t-sm"
+			/>
+			<div
+				class="flex self-end bg-zinc-500 hover:bg-zinc-300 w-3 h-5 rounded-t-sm"
+			/>
+			<div
+				class="flex self-end bg-zinc-500 hover:bg-zinc-300 w-3 h-4 rounded-t-sm"
+			/>
+			<div
+				class="flex self-end bg-zinc-500 hover:bg-zinc-300 w-3 h-2 rounded-t-sm"
+			/>
+		</div>
+		<div class="flex mx-auto">
+			<div class="flex flex-col">
+				<p class="text-zinc-400 mx-auto text-lg">3.5</p>
+				<div class="flex flex-row">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="w-3 h-4 text-green-400"
+						viewBox="0 0 20 20"
+						fill="currentColor"
+					>
+						<path
+							d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+						/>
+					</svg>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="w-3 h-4 text-green-400"
+						viewBox="0 0 20 20"
+						fill="currentColor"
+					>
+						<path
+							d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+						/>
+					</svg>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="w-3 h-4 text-green-400"
+						viewBox="0 0 20 20"
+						fill="currentColor"
+					>
+						<path
+							d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+						/>
+					</svg>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="w-3 h-4 text-green-400"
+						viewBox="0 0 20 20"
+						fill="currentColor"
+					>
+						<path
+							d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+						/>
+					</svg>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="w-3 h-4 text-green-400"
+						viewBox="0 0 20 20"
+						fill="currentColor"
+					>
+						<path
+							d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+						/>
+					</svg>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
