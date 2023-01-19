f7c6bd2ac2abc27c78b13f7ab9a9b95136e3a22c
grantgariepy <grantgariepy@protonmail.com>
2023-01-18 20:31:11 -0700

    stats under book cover

--- src/routes/book/BookSlug/components/BookCover.svelte
+++ src/routes/book/BookSlug/components/BookCover.svelte
@@ -59,10 +59,68 @@
 		</div>
 		<!-- overlay end -->
 	</a>
-	<div class="flex justify-evenly text-zinc-200 pb-6">
-		<p>watched</p>
-		<p>saved</p>
-		<p>review</p>
+	<div
+		class="flex flex-row justify-center gap-3 text-zinc-200 pb-6"
+	>
+		<div class="flex flex-row gap-1">
+			<svg
+				fill="currentColor"
+				class="w-4 h-4 text-green-400 self-center group-hover:text-white transition duration-150"
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 512.001 512.001"
+			>
+				<path
+					d="M256,192.012c-35.313,0-64.002,28.688-64.002,64s28.689,63.969,64.002,63.969s64-28.656,64-63.969 S291.313,192.012,256,192.012z"
+				/>
+				<path
+					d="M505.381,236.512c-56.082-72.938-125.065-140.438-249.225-140.5h-0.016h-0.016c0-0.031-0.094-0.031-0.125,0 c0,0-0.078,0-0.109,0c-0.016,0-0.016,0-0.016,0h-0.016c-0.016,0-0.016,0-0.016,0C131.684,96.075,62.7,163.575,6.62,236.512 c-8.826,11.5-8.826,27.5,0,39c56.08,72.938,125.065,140.438,249.225,140.5c0,0,0,0,0.016,0h0.016c0,0,0,0,0.016,0h0.016 c0.016,0,0.094,0,0.094,0c0.016,0,0.031,0,0.064,0h0.029h0.016h0.016h0.016h0.016c0.109,0,0.219,0,0.344,0 c0.141-0.031,0.281-0.031,0.422,0c123.66-0.336,192.486-67.719,248.459-140.5C514.207,264.012,514.207,248.012,505.381,236.512z M256.094,352.012h-0.018h-0.012h-0.018h-0.031H256c-0.031,0-0.065,0-0.065,0h-0.014c0,0,0,0-0.016,0 c-52.879-0.063-95.908-43.125-95.908-96s43.029-95.938,95.908-96c0.029,0,0.078,0,0.141,0h0.047 c52.877,0.063,95.906,43.125,95.906,96S308.971,351.95,256.094,352.012z"
+				/>
+			</svg>
+			<a
+				href="/"
+				class="text-xs text-zinc-400 hover:text-zinc-200"
+				>203k</a
+			>
+		</div>
+		<div class="flex flex-row gap-1">
+			<svg
+				class="w-4 h-4 text-blue-400 self-center group-hover:text-white transition duration-150"
+				viewBox="0 0 48 48"
+				xmlns="http://www.w3.org/2000/svg"
+				fill="currentColor"
+			>
+				<path d="M0 0h48v48H0z" fill="none" />
+				<g id="Shopicon">
+					<rect x="4" y="4" width="18" height="18" />
+					<rect x="26" y="4" width="18" height="18" />
+					<rect x="4" y="26" width="18" height="18" />
+					<rect x="26" y="26" width="18" height="18" />
+				</g>
+			</svg>
+			<a
+				href="/"
+				class="text-xs text-zinc-400 hover:text-zinc-200"
+				>44k</a
+			>
+		</div>
+		<div class="flex flex-row gap-1">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				class="w-4 h-4 text-orange-400 self-center group-hover:text-white transition duration-150"
+				viewBox="0 0 512 512"
+				fill="currentColor"
+			>
+				<path
+					class="st0"
+					d="M473.984,74.248c-50.688-50.703-132.875-50.703-183.563,0c-17.563,17.547-29.031,38.891-34.438,61.391 c-5.375-22.5-16.844-43.844-34.406-61.391c-50.688-50.703-132.875-50.703-183.563,0c-50.688,50.688-50.688,132.875,0,183.547 l217.969,217.984l218-217.984C524.672,207.123,524.672,124.936,473.984,74.248z"
+				/>
+			</svg>
+			<a
+				href="/"
+				class="text-xs text-zinc-400 hover:text-zinc-200"
+				>97k</a
+			>
+		</div>
 	</div>
 </div>
 <!-- product - end -->
