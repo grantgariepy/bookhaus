c85c29a0e268a5546472e5510f0c92efaae1fae5
grantgariepy <grantgariepy@protonmail.com>
2023-01-19 19:41:51 -0700

    stats

--- src/routes/profile/components/UserInfo.svelte
+++ src/routes/profile/components/UserInfo.svelte
@@ -1,55 +1,130 @@
 <div class="bg-zinc-800 lg:pb-2 z-0">
 	<div class="max-w-screen-lg px-4 md:px-8 mx-auto ">
-		<div class="flex flex-row p-6 gap-5">
-			<div
-				class=" md:w-24 md:h-24 bg-gray-100 rounded-full overflow-hidden shadow-lg "
-			>
-				<img
-					src="https://images.unsplash.com/photo-1567515004624-219c11d31f2e??auto=format&q=75&fit=crop&w=256"
-					loading="lazy"
-					alt="by Radu Florin"
-					class="w-full h-full object-cover object-center"
-				/>
-			</div>
-			<div class="flex flex-col gap-2">
-				<div class="flex flex-row gap-2">
-					<p class="text-white text-2xl font-bold ">
-						garnt
+		<div
+			class="md:flex flex-row pl-6 pt-8 pb-4 justify-between"
+		>
+			<div class="md:flex flex-row gap-5">
+				<div
+					class="w-16 h-16 md:w-24 md:h-24 bg-gray-100 rounded-full overflow-hidden shadow-lg "
+				>
+					<img
+						src="https://images.unsplash.com/photo-1567515004624-219c11d31f2e??auto=format&q=75&fit=crop&w=256"
+						loading="lazy"
+						alt="by Radu Florin"
+						class="w-full h-full object-cover object-center"
+					/>
+				</div>
+				<div class="md:flex flex-col gap-2">
+					<div class="flex flex-row gap-2">
+						<p class="text-white text-2xl font-bold ">
+							garnt
+						</p>
+						<div class="self-center">
+							<p
+								class="text-white text-xs bg-orange-500 rounded-sm min-h-min px-1"
+							>
+								PRO
+							</p>
+						</div>
+						<div class="pl-5 self-center group">
+							<button
+								class=" bg-green-500 hover:bg-orange-500 text-white text-sm font-medium tracking-wide rounded px-3 py-0.5 uppercase"
+								>Following</button
+							>
+						</div>
+						<div class="self-center pl-3">
+							<p class="text-zinc-400 text-sm">
+								Follows You
+							</p>
+						</div>
+					</div>
+					<p class="text-white text-sm font-thin ">
+						feed me a stray cat
 					</p>
-					<div class="self-center">
-						<p
-							class="text-white text-xs bg-orange-500 rounded-sm min-h-min px-1"
+					<div class="flex flex-row gap-2">
+						<div
+							class=" md:w-6 md:h-6 bg-gray-100 rounded-full overflow-hidden shadow-lg "
 						>
-							PRO
+							<img
+								src="https://images.unsplash.com/photo-1567515004624-219c11d31f2e??auto=format&q=75&fit=crop&w=256"
+								loading="lazy"
+								alt="by Radu Florin"
+								class="w-full h-full object-cover object-center"
+							/>
+						</div>
+						<p class="text-xs text-zinc-400 self-center">
+							Followed by username69
 						</p>
 					</div>
-					<div class="pl-5 self-center group">
-						<button
-							class=" bg-green-500 hover:bg-orange-500 text-white text-sm font-medium tracking-wide rounded px-3 py-1 uppercase"
-							>Following</button
-						>
-					</div>
-					<div class="self-center pl-3">
-						<p class="text-zinc-400 text-sm">Follows You</p>
-					</div>
 				</div>
-				<p class="text-white text-sm font-thin ">
-					feed me a stray cat
-				</p>
-				<div class="flex flex-row gap-2">
-					<div
-						class=" md:w-6 md:h-6 bg-gray-100 rounded-full overflow-hidden shadow-lg "
+			</div>
+			<div
+				class="grid grid-cols-5 self-end divide-x divide-zinc-500 pb-2"
+			>
+				<div class="flex flex-col gap-1 ">
+					<p
+						class="text-white text-2xl font-serif self-center justify-center"
 					>
-						<img
-							src="https://images.unsplash.com/photo-1567515004624-219c11d31f2e??auto=format&q=75&fit=crop&w=256"
-							loading="lazy"
-							alt="by Radu Florin"
-							class="w-full h-full object-cover object-center"
-						/>
-					</div>
-					<p class="text-xs text-zinc-400 self-center">
-						Followed by username69
+						530
 					</p>
+					<a
+						href="/"
+						class="text-zinc-500 text-xs self-center justify-center uppercase hover:text-blue-500"
+					>
+						books
+					</a>
+				</div>
+				<div class="flex flex-col gap-1 px-2">
+					<p
+						class="text-white text-2xl font-serif self-center justify-center"
+					>
+						12
+					</p>
+					<a
+						href="/"
+						class="text-zinc-500 text-xs self-center justify-center uppercase hover:text-blue-500"
+					>
+						This Year
+					</a>
+				</div>
+				<div class="flex flex-col gap-1">
+					<p
+						class="text-white text-2xl font-serif self-center justify-center "
+					>
+						1
+					</p>
+					<a
+						href="/"
+						class="text-zinc-500 text-xs self-center justify-center uppercase hover:text-blue-500"
+					>
+						List
+					</a>
+				</div>
+				<div class="flex flex-col gap-1 ">
+					<p
+						class="text-white text-2xl font-serif self-center justify-center"
+					>
+						23
+					</p>
+					<a
+						href="/"
+						class="text-zinc-500 text-xs self-center justify-center uppercase hover:text-blue-500"
+					>
+						Folowing
+					</a>
+				</div>
+				<div class="flex flex-col gap-1">
+					<p
+						class="text-white text-2xl font-serif self-center justify-center"
+					>
+						26
+					</p>
+					<a
+						href="/"
+						class="text-zinc-500 text-xs self-center justify-center uppercase hover:text-blue-500"
+					>
+						Followers
+					</a>
 				</div>
 			</div>
 		</div>
