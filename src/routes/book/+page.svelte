b96d907cc1fbe8339bb1f8a1b19697d55264c273
grantgariepy <grantgariepy@protonmail.com>
2023-02-03 19:57:03 -0700

    header import fix

--- src/routes/book/+page.svelte
+++ src/routes/book/+page.svelte
@@ -1,14 +1,11 @@
 <script>
 	import { page } from '$app/stores';
-	import BooksHeader from '../components/BooksHeader.svelte';
-	import LoggedInHeader from '../components/LoggedIn/LoggedInHeader.svelte';
-	import LoggedInBookHeader from './BookSlug/components/LoggedInBookHeader.svelte';
-	import LoggedOutBookHeader from './BookSlug/components/LoggedOutBookHeader.svelte';
+	// import BooksHeader from '../components/BooksHeader.svelte';
+	// import LoggedInHeader from '../components/LoggedIn/LoggedInHeader.svelte';
+	// import LoggedInBookHeader from './BookSlug/components/LoggedInBookHeader.svelte';
+	import LoggedOutBookHeader from './[bookSlug]/components/LoggedOutBookHeader.svelte';
 </script>
 
-{#if $page.data.session}
-	<LoggedInBookHeader />
-{:else}
-	<LoggedOutBookHeader />
-{/if}
+<LoggedOutBookHeader />
+
 <h1>sorry we can't find this page</h1>
